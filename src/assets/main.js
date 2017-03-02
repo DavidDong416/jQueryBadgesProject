f7e20a079c158defce09784ef83ebc6798370387
DavidDong416 <dk@Kes-MacBook-Pro.local>
2017-03-02 10:49:09 -0500

    jQuery task accomplish for the project

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,10 +4,46 @@ $(function() {
       dataType: 'jsonp',
       success: function(response) {
         // handle response
-        console.log(response);
+        // console.log(response.courses.completed);
+        addCourses(response.courses.completed);
 
+      },
+      error: function() {
+        errorMessage();
       }
+
+
     });
-  // your code will go here
+
+    function addCourses(courses){
+      var $badges = $('#badges');
+
+      courses.forEach(function(course){
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: "See Course"
+        }).appendTo($course);
+      })
+    }
+
+    function errorMessage(){
+      $('<p />', {
+        text: "We cannot get the badage from the codeschool server."
+      }).appendTo('.container');
+    }
 
 });
